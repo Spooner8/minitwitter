bf5ca0f75f2cdf1cf4139e24868a686689d609c8
Spooner <spooner@mmcs.ch>
2025-01-08 18:28:55 +0100

    test integrations

--- src/services/crud/posts.ts
+++ src/services/crud/posts.ts
@@ -2,6 +2,9 @@ import ollama from 'ollama';
 import { postsTable } from '../../schemas';
 import { db } from '../database.ts';
 import { eq } from 'drizzle-orm';
+import 'dotenv/config';
+
+const OLLAMA_BASE_URL = process.env.OLLAMA_BASE_URL || 'http://localhost:11434';
 
 export const postService = {
     getPosts,
@@ -30,23 +33,41 @@ async function createPost(userId: number, content: string) {
 }
 
 async function generatePost(userId: number) {
-    const content = await ollama.chat({
-        model: 'llama3.2:1b',
-        messages: [
-            {
-                role: 'user',
-                content:
-                    'Create a short random twitter-post with maximum 140 characters.',
-            },
-        ],
-    });
+    // const content = await ollama.chat({
+    //     model: 'llama3.2:1b',
+    //     messages: [
+    //         {
+    //             role: 'user',
+    //             content:
+    //                 'Create a short random twitter-post with maximum 140 characters.',
+    //         },
+    //     ],
+    // });
 
-    const post: typeof postsTable.$inferInsert = {
-        userId: userId,
-        content: content.message.content,
-    };
+    console.log('fetching content for ollama');
+    const content = await fetch(`${OLLAMA_BASE_URL}/chat`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            model: 'llama3.2:1b',
+            messages: [
+                {
+                    role: 'user',
+                    content:
+                        'Create a short random twitter-post with maximum 140 characters.',
+                },
+            ],
+        }),
+    }).then((res) => console.log(res));
+    return true;
+    // const post: typeof postsTable.$inferInsert = {
+    //     userId: userId,
+    //     content: content.message.content,
+    // };
 
-    return await db.insert(postsTable).values(post).returning();
+    // return await db.insert(postsTable).values(post).returning();
 }
 
 async function updatePost(id: number, content: string) {
