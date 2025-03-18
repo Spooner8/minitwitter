7fc1685c1d5b560160b03284b879a45f0c681645
Spooner8 <develop@swen-loeffler.ch>
2025-03-18 22:11:02 +0100

    finaly setup eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,39 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-
+import { globalIgnores } from "eslint/config";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  globalIgnores(
+    [
+      "**/*.d.ts",
+      "**/*.{cjs,mjs}",
+    ]
+  ),
   {
-    files: ["**/*.{js,mjs,cjs,ts,vue}"]
-  },
-  {
-    rules: {
-      'vue/multi-word-component-names': 'off',
-      'vue/no-textarea-mustache': 'off',
-      'no-unused-expressions': 'off',
-      'no-unused-vars': 'off',
-      'no-explicit-any': 'off',
+    languageOptions: { 
+      globals: globals.browser,
+      parserOptions: {
+        parser: tseslint.parser
+      }
     },
-  },
-  {
-    languageOptions: { globals: globals.browser }
   },
   ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/essential"],
   {
-    files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}
-  },
-  {
+    files: ["**/*.vue"],
     rules: {
       'vue/multi-word-component-names': 'off',
       'vue/no-textarea-mustache': 'off',
-      'no-unused-expressions': 'off',
-      'no-unused-vars': 'off',
-      'no-explicit-any': 'off',
+    },
+  },
+  {
+    files: ["**/*.{js,ts,vue}"],
+    rules: {
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
     },
   },
 ];
